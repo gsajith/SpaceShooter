be328a4bcc9dae449d82d8317e7f7b0ed52171d1
Chermine Stanley-Nasr <cnsn@umich.edu>
2014-01-18 17:04:08 -0500

    organization

---
+++ Assets/Menu Scripts/holdColor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class holdColor : MonoBehaviour {
+	static public Color hold;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
