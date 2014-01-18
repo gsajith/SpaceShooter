886d2c1d7f4177b8e8ca8d6228bc0d7d221d2f5a
gsajith <gsajith@umich.edu>
2014-01-18 15:43:47 -0500

    Menu Screen
    Particles pew pew

---
+++ Assets/Scripts/LogoSpinScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogoSpinScript : MonoBehaviour {
+	public float xRot, yRot, zRot;
+	float rotAmount = 90.0f;
+	public GameObject particles;
+	void Update() {
+		transform.Rotate (xRot*rotAmount * Time.deltaTime, 
+		                  yRot*rotAmount * Time.deltaTime, 
+		                  zRot*rotAmount * Time.deltaTime);
+
+	}
+
+	void OnMouseEnter() {
+		GameObject newParticles = (GameObject)Instantiate (particles, transform.position, new Quaternion (0, 0, 0, 0));
+		Destroy (newParticles, 1);
+
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+}
