c017618e0469b47c4cd89bbc9a97b5633759b70a
ngbenny <bennyng@umich.edu>
2014-01-18 18:23:38 -0500

    Update

--- Assets/EnemyShoot.cs
+++ Assets/EnemyShoot.cs
@@ -3,5 +3,15 @@ using System.Collections;
 
 public class EnemyShoot : MonoBehaviour {
 	public GameObject shot;
+	public float firerate;
+	private float nextshot = 0.0F;
 
+	void Update(){
+		if (Time.time > nextshot) {
+			nextshot = firerate+Time.time;
+			Instantiate (shot, transform.position, transform.rotation);
+
+		}
+
+	}
 }
