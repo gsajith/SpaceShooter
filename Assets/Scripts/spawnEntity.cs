b3b34a48bc8059ebc3cbb53d1076de6a49bac520
ngbenny <bennyng@umich.edu>
2014-01-18 16:53:42 -0500

    Minor Update

--- Assets/Scripts/spawnEntity.cs
+++ Assets/Scripts/spawnEntity.cs
@@ -5,10 +5,13 @@ public class spawnEntity : MonoBehaviour {
 
 	public GameObject entity;
 	public float spawnrate;
+	public bool isRespawning;
 	float t = Time.time;
-
+	void Start(){
+		Instantiate (entity, transform.position, transform.rotation);
+	}
 	void Update(){
-		if (Time.time >= spawnrate + t)
+		if (isRespawning && (Time.time >= spawnrate + t))
 			Instantiate (entity, transform.position, transform.rotation);
 			t = Time.time;
 		}
