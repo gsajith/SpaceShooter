a1e6d8ebe9d4303ed34a64405b97d7794d5cc13b
ngbenny <bennyng@umich.edu>
2014-01-18 16:22:58 -0500

    Remade Spawn points(WIP)

---
+++ Assets/Scripts/spawnEntity.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawnEntity : MonoBehaviour {
+
+	public GameObject entity;
+	public float spawnrate;
+	float t = Time.time;
+
+	void Update(){
+		if (Time.time >= spawnrate + t)
+			Instantiate (entity, transform.position, transform.rotation);
+			t = Time.time;
+		}
+}
