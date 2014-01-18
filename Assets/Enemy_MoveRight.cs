6d2335cac0094250aba3d322f22d172c95846a97
ngbenny <bennyng@umich.edu>
2014-01-18 02:55:35 -0500

    Here we go...

---
+++ Assets/Enemy_MoveRight.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_MoveRight : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	public float maxEnemies;
+	public float speed;
+	public GameObject Enemy;
+	float count = 0;
+	// Update is called once per frame
+	//var projectile : Rigidbody2D;
+
+	void Update () {
+	/*	var clone : Rigidbody2D;
+		clone = Instantiate (projectile, transform.positon, transform.rotation);
+		clone.velocity = transform.TransformDirection(Vector2.right);
+	*/}
+}
