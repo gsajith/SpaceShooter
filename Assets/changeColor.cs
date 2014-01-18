f2f0a296480add6e14103ffc904b489f2816ee9b
gsajith <gsajith@umich.edu>
2014-01-18 16:53:31 -0500

    Changed customize screen stuff
    Double click to drag!

--- Assets/changeColor.cs
+++ Assets/changeColor.cs
@@ -4,14 +4,22 @@ using System.Collections.Generic;
 public class changeColor : MonoBehaviour {
 
 	public bool isPaint;
+	public bool canDrag;
 	void Start() {
 
 	}
 	void OnMouseDown() {
-		//GameObject clone = Instantiate((Object)this, this.transform.position, this.transform.rotation);
-		//MoveBlock blocky = new MoveBlock();
-		//blocky.canMove = true;
-		//clone.AddComponent (blocky);
+		if(canDrag) {
+			GameObject original = this.gameObject;
+			Vector3 pos = original.transform.position;
+			pos.y = pos.y + .01f;
+			GameObject clone = Instantiate(original, pos, original.transform.rotation) as GameObject;
+			MoveBlock script = clone.GetComponent<MoveBlock> ();
+			script.canMove = true;
+			changeColor colorScript = clone.GetComponent<changeColor>();
+			colorScript.canDrag = false;
+		}
+
 		if (isPaint) {
 			renderer.material.color = holdColor.hold;
 		} else {
