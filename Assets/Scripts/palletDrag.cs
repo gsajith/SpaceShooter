5430b38d751266ec7cd7cf747f049e2d043ba1a2
Chermine Stanley-Nasr <cnsn@umich.edu>
2014-01-18 22:12:52 -0500

    Steps towards making a custom ship!

--- Assets/Scripts/palletDrag.cs
+++ Assets/Scripts/palletDrag.cs
@@ -5,6 +5,7 @@ public class palletDrag : MonoBehaviour {
 
 	public bool isPaint;
 	public bool canDrag;
+	public GameObject ShipPrefab;
 	void Start() {
 
 	}
@@ -14,10 +15,24 @@ public class palletDrag : MonoBehaviour {
 			Vector3 pos = original.transform.position;
 			pos.y = pos.y + .01f;
 			GameObject clone = Instantiate(original, pos, original.transform.rotation) as GameObject;
+			clone.transform.parent = ShipPrefab.transform;
 			MoveBlock script = clone.GetComponent<MoveBlock> ();
 			script.canMove = true;
+
 			palletDrag colorScript = clone.GetComponent<palletDrag>();
 			colorScript.canDrag = false;
+
+			ShipPart portion = new ShipPart();
+			portion.obj = clone;
+
+			float gridx = (pos.x + 2f)*2f; 
+			float gridz = (pos.z + 2f)*2f;
+			Vector2 position = new Vector2(gridx, gridz);
+			portion.loc = position;
+
+			int type = 0;
+
+			ShipMakerScript.shipParts.Add(portion);
 		}
 	}
 
