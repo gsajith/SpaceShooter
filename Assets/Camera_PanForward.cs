a094cc561b69f3b34a65f9679bc48f0ace5a18a6
ngbenny <bennyng@umich.edu>
2014-01-18 22:19:45 -0500

    push

---
+++ Assets/Camera_PanForward.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_PanForward : MonoBehaviour {
+
+	//public float speedMult = 0;
+
+	void Update(){
+		this.transform.Translate (Vector3.up * Time.deltaTime, Space.World);
+	}
+}
---
+++ Assets/Scripts/Enemy/Enemy_Movement1.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_Movement1 : MonoBehaviour {
+	public float x, y;
+	public Vector2 direction;
+	public float speed;
+	private Vector2 movement;
+
+	void Update(){
+		movement = new Vector2 (speed * direction.x, (Mathf.Cos(direction.x * Mathf.Deg2Rad)*(1/2)));
+		}
+
+	void FixedUpdate(){
+		this.rigidbody2D.velocity = movement;
+	}
+
+}
