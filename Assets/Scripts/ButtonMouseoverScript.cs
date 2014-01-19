0fa8d586d089e7e3be30585975acd3e7c90af565
gsajith <gsajith@umich.edu>
2014-01-19 04:25:14 -0500

    New scene for game, collision working

--- Assets/Scripts/ButtonMouseoverScript.cs
+++ Assets/Scripts/ButtonMouseoverScript.cs
@@ -8,6 +8,7 @@ public class ButtonMouseoverScript : MonoBehaviour {
 	public Sprite mouseoverSprite;
 	public GameObject particles;
 	public GameObject playerPrefab;
+	public bool makePrefab;
 	public int sceneNum;
 
 	void OnMouseEnter() {
@@ -26,10 +27,21 @@ public class ButtonMouseoverScript : MonoBehaviour {
 
 	IEnumerator OnMouseUp(){
 		yield return new WaitForSeconds(1);
-		var location = "Assets/PlayerPrefab.prefab";
-		PrefabUtility.CreatePrefab(location, 
-		                           playerPrefab, 
-		                           ReplacePrefabOptions.ReplaceNameBased);
+		if(makePrefab){
+			palletDrag[] drags = playerPrefab.GetComponentsInChildren<palletDrag>();
+			foreach(palletDrag drag in drags) {
+				drag.canDrag = false;
+			}
+			MoveBlock[] moveBlocks = playerPrefab.GetComponentsInChildren<MoveBlock>();
+			foreach(MoveBlock moveBlock in moveBlocks) {
+				moveBlock.canMove = false;
+			}
+			var location = "Assets/PlayerPrefab.prefab";
+			PrefabUtility.CreatePrefab(location, 
+			                           playerPrefab, 
+			                           ReplacePrefabOptions.ReplaceNameBased);
+
+		}
 		//Application.LoadLevel(sceneNum);
 		
 	}
