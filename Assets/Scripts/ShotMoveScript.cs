e80a3dc549c78a4ac70fae8be223f5d054fa6af4
gsajith <gsajith@umich.edu>
2014-01-18 21:31:18 -0500

    Killin' dem bunnies
    yeeeeeeeeeeeeee

--- Assets/Scripts/ShotMoveScript.cs
+++ Assets/Scripts/ShotMoveScript.cs
@@ -14,6 +14,8 @@ public class ShotMoveScript : MonoBehaviour {
 	/// Moving direction
 	/// </summary>
 	public Vector2 direction = new Vector2(-1, 0);
+
+	public float damage = 1.0f;
 	
 	private Vector2 movement;
 
@@ -24,8 +26,9 @@ public class ShotMoveScript : MonoBehaviour {
 	}
 	
 	void OnTriggerEnter2D(Collider2D other) {
-		if(!(other.isTrigger)) {
-			Debug.Log ("here");
+		Enemy_MoveDirection enemyScript = other.GetComponent<Enemy_MoveDirection> ();
+		if(enemyScript != null) {
+			enemyScript.health -= damage;
 			Destroy (other.gameObject);
 			Destroy (this.gameObject);
 		}
