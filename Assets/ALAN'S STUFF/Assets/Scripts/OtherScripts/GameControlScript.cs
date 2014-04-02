8fd159b3a5637994f137f0c4afbe27f53c3f00e2
AlanKong <lingkong@umich.edu>
2014-04-02 17:29:49 -0400

    alanupdate
    alanupdate

---
+++ Assets/ALAN'S STUFF/Assets/Scripts/OtherScripts/GameControlScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControlScript : MonoBehaviour {
+
+	static public bool select = false;
+	static public GameObject selectedPlanet = null;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+}
---
+++ Assets/ALAN'S STUFF/Assets/Scripts/PlanetControlScripts/OccupyScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OccupyScript : MonoBehaviour {
+
+	public Sprite userGreen;
+	public Sprite enemyRed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (transform.parent.GetComponent<PlanetStatsScript> ().occupy == 1) {
+						GetComponent<SpriteRenderer> ().sprite = userGreen;
+				} else if (transform.parent.GetComponent<PlanetStatsScript> ().occupy == 2) {
+						GetComponent<SpriteRenderer> ().sprite = enemyRed;
+				} else {
+			GetComponent<SpriteRenderer> ().sprite = null;
+				}
+	
+	}
+}
