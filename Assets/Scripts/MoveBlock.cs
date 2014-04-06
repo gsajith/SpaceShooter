43a9d1613d51d908b63532b44ecced46665af9f9
Joe <jothwarr@umich.edu>
2014-04-06 16:35:15 -0400

    added "buying box", working model of ship parts

--- Assets/Scripts/MoveBlock.cs
+++ Assets/Scripts/MoveBlock.cs
@@ -5,6 +5,7 @@ using System;
 public class MoveBlock : MonoBehaviour {
 	public bool canMove;
 	private bool instantiated = true;
+	public MultidimensionalInt[] shipModel = new MultidimensionalInt[10];
 
 	// Use this for initialization
 	void Start () {
@@ -42,9 +43,30 @@ public class MoveBlock : MonoBehaviour {
 
 			if(point.x <= -2.25f || point.x >= 2.25f
 			   || point.y <= -1.25f || point.y >= 3.25f)
-				DestroyObject(gameObject);
+				{
+				point.x = 0f;
+				point.y = 3.8f;
+				point.z = 0f;
+				gameObject.transform.position = point;
+				//GUI.Label(new Rect(-3,3,200,100), "Error placing component");
+				//square is occupied, dont let them buy or place other parts
+			}
 			else
-			gameObject.transform.position = point;
+			{
+				int modelX = (int)(2*(point.x) + 4.05f);
+				int modelY = (int)(2*(point.y) + 2.05f);
+				Debug.Log (modelX + ", " + modelY);
+
+				ShipPart copy = gameObject.GetComponent<ShipPart>();
+				if(copy != null){
+					if(modelX >= 0 && modelX <= 8 && modelY >= 0 && modelY <= 8)
+					{
+						shipModel[modelX].intArray = new int[9];
+						shipModel[modelX][modelY] = copy.type; //portion.type.ToString();
+					}
+				}
+				gameObject.transform.position = point;
+			}
 
 		}
 
--- Assets/Scripts/ShipMakerScript.cs
+++ Assets/Scripts/ShipMakerScript.cs
@@ -5,11 +5,9 @@ using System;
 public class ShipMakerScript : MonoBehaviour {
 
 	static public ArrayList shipParts = new ArrayList();
+
 	// Use this for initialization
 	void Start () {
 
-	}
-	// Update is called once per frame
-	void Update () {
 	}
 }
